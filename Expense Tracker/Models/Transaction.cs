862c64fda3f7e6dceae185873e04d69b34a982d9
Sadiqul Sakif <s4ki3f@gmail.com>
2023-08-25 17:29:56 +0600

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryID
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public String? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
