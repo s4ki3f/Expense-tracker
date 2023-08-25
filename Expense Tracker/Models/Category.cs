17fe0bbd0ab83b26efff8783c5b2dfccbe2d6f81
Sadiqul Sakif <s4ki3f@gmail.com>
2023-08-25 21:42:18 +0600

    index changes, model and views chnages

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -9,12 +9,22 @@ namespace Expense_Tracker.Models
         public int CategoryId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
 
         [Column(TypeName = "nvarchar(5)")]
-        public string Icon { get; set; } = string.Empty;
+        public string Icon { get; set; } = "";
 
         [Column(TypeName = "nvarchar(10)")]
-        public string Type { get; set; } = "Expanse";
+        public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
